84f3f6b4ff05c9c36a45d4b37b8debc695ea99b6
ManuelDev24 <msoliveymedrano@gmail.com>
2024-08-08 18:42:14 -0400

    Add project files.

---
+++ UbicacionGPS/UbicacionGPS/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace UbicacionGPS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Código que se ejecuta cuando la aplicación se inicia
+        }
+
+        protected override void OnSleep()
+        {
+            // Código que se ejecuta cuando la aplicación entra en modo de suspensión
+        }
+
+        protected override void OnResume()
+        {
+            // Código que se ejecuta cuando la aplicación vuelve a estar activa
+        }
+    }
+}
---
+++ UbicacionGPS/UbicacionGPS/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+using Xamarin.Essentials;
+using System;
+
+namespace UbicacionGPS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtLatitud.Text, out double lat))
+            {
+                await DisplayAlert("Error", "Por favor ingresa una latitud válida.", "OK");
+                return;
+            }
+
+            if (!double.TryParse(txtLongitud.Text, out double lng))
+            {
+                await DisplayAlert("Error", "Por favor ingresa una longitud válida.", "OK");
+                return;
+            }
+
+            // Abre Google Maps con la ubicación proporcionada
+            await Map.OpenAsync(lat, lng, new MapLaunchOptions
+            {
+                Name = txtNombreUbicacion.Text,
+                NavigationMode = NavigationMode.None
+            });
+        }
+    }
+}
